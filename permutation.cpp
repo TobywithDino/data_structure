1714030266934e546b33675bbafa315df2d35ed1
TobywithDino <penguinoftoby@gmail.com>
2023-11-06 10:47:50 +0800

    cool

--- permutation.cpp
+++ permutation.cpp
@@ -2,28 +2,28 @@
 #include <cstring>
 using namespace std;
 
-void permutation_recursive(char* list, int i, int n){
-    if(i==n){
-        for(int j=0;j<n;j++){
-            cout << list[j];
-        }
-        cout << endl;
-    }else{
-        for(int j=i;j<n;j++){
-            swap(list[i],list[j]);
-            permutation_recursive(list, i+1, n);
-            swap(list[i],list[j]);
+void permutation(char *list, int i, int n)
+{
+    if (i == n)
+    {
+        printf("%s\n", list);
+    }
+    else
+    {
+        for (int j = i; j < n; j++)
+        {
+            swap(list[i], list[j]);
+            permutation(list, i + 1, n);
+            swap(list[i], list[j]);
         }
     }
 }
 
-int main(){
-    string tmp;
-    cin >> tmp;
-    int n = tmp.length();
-    char *list = new char[n];
-    strcpy(list, tmp.c_str());
-    cout << "=================\n";
-    permutation_recursive(list, 0, n);
-    cout << "=================\n";
+int main()
+{
+    char list[100];
+    string s;
+    cin >> s;
+    strcpy(list, s.c_str());
+    permutation(list, 0, s.length());
 }
