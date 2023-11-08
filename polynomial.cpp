a25c4f43e9bf2fe2c20cc418d9c31dc7561480c7
TobywithDino <penguinoftoby@gmail.com>
2023-11-08 15:36:05 +0800

    Cool

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
 using namespace std;
 
-struct node{
+struct node
+{
     int coef;
     int degree;
-    node* next;
+    node *next;
 };
 
-void insert(node* ptr, int coef, int degree){
-    node* head = ptr;
-    while(ptr->next != nullptr){
+void insert(node *ptr, int coef, int degree)
+{
+    while (ptr->next != nullptr)
+    {
         ptr = ptr->next;
     }
     node *tmp = new node{coef, degree, nullptr};
-    ptr->next =  tmp;
-    ptr = head;
+    ptr->next = tmp;
 }
 
-node* combine(node* a, node* b){
-    node* head = new node;
-    node* cur = new node;
+node *combine(node *a, node *b)
+{
+    node *head = new node;
+    node *cur = new node;
     head = cur;
-    while(a!=nullptr && b!=nullptr){
-        node* tmp = new node;
+    while (a != nullptr && b != nullptr)
+    {
+        node *tmp = new node;
         tmp->next = nullptr;
         cur->next = tmp;
-        if(a->degree > b->degree){
+        if (a->degree > b->degree)
+        {
             tmp->degree = a->degree;
             tmp->coef = a->coef;
             a = a->next;
         }
-        else if(a->degree < b->degree){
+        else if (a->degree < b->degree)
+        {
             tmp->degree = b->degree;
             tmp->coef = b->coef;
             b = b->next;
         }
-        else{
+        else
+        {
             tmp->degree = a->degree;
             tmp->coef = a->coef + b->coef;
             a = a->next;
@@ -44,20 +50,23 @@ node* combine(node* a, node* b){
         cur = cur->next;
     }
 
-    while(a!=nullptr){
-        insert(head, a->coef,a->degree);
+    while (a != nullptr)
+    {
+        insert(head, a->coef, a->degree);
         a = a->next;
     }
-    while(b!=nullptr){
-        insert(head, b->coef,b->degree);
+    while (b != nullptr)
+    {
+        insert(head, b->coef, b->degree);
         b = b->next;
     }
 
-    return head->next; 
+    return head->next;
 }
 
-int main(){
-    node* a = new node{13,10,nullptr};
+int main()
+{
+    node *a = new node{13, 10, nullptr};
     insert(a, 32, 9);
     insert(a, 71, 8);
     insert(a, 50, 6);
@@ -67,14 +76,15 @@ int main(){
     insert(a, 7, 1);
     insert(a, 21, 0);
 
-    node* b = new node{3,9,nullptr};
+    node *b = new node{3, 9, nullptr};
     insert(b, 13, 8);
     insert(b, -3, 6);
     insert(b, 20, 4);
 
-    node* result = combine(a, b);
-    while(result != nullptr){
-        cout << result->coef << "X^"<< result->degree << endl;
+    node *result = combine(a, b);
+    while (result != nullptr)
+    {
+        cout << result->coef << "X^" << result->degree << endl;
         result = result->next;
     }
 }
---
+++ practice_queue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+int main()
+{
+    node *queue = new node;
+}
